db2fe6010b71194a841269cd7442c2a3c6e22912
Shane Kovacs <kovacsthesmith@outlook.com>
2023-03-12 11:58:48 +1100

    initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,9 @@
+def load_urls_from_file(file_path: str):
+    #todo: add the code needed to read the text file with the urls in it
+    pass
+def load_page(url: str):
+    #todo: add code that reads the url
+    pass
+def scrape_page(page_contents: str)
+    #todo: analyze the text
+    pass
